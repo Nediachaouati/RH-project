caff8bd1ccd8b48981bfa255e5930b4cc4c71266
Talel-oueslati <taleloueslati3@gmail.com>
2025-03-03 17:15:44 +0100

    add authentification with home page and roles pages

---
+++ frontend/src/app/admin-components/rh-list/rh-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rh-list',
+  imports: [],
+  templateUrl: './rh-list.component.html',
+  styleUrl: './rh-list.component.css'
+})
+export class RhListComponent {
+
+}
